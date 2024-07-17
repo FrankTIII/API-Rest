f5357f83760b9cd07fde33da5b8b9ff20bc52f48
FrankTIII <frank_training@outlook.com>
2024-07-16 19:05:26 -0600

    1st commint: Connect2 Repo

---
+++ src/main/java/com/allura/APIREst/ApirEstApplication.java
@@ -0,0 +1,13 @@
+package com.allura.APIREst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirEstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirEstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allura/APIREst/ApirEstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allura.APIREst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirEstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
